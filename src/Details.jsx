4b7586ad1cf2731ab2970109ba8978919eb8a602
sarmaakondi <avnmsarma@gmail.com>
2024-12-25 16:34:44 +1100

    Create Details placeholder component

---
+++ src/Details.jsx
@@ -0,0 +1,9 @@
+const Details = () => {
+    return (
+        <div className="flex flex-col p-8 mt-12 mb-12 border border-black w-[672px]">
+            <h1>Details</h1>
+        </div>
+    );
+};
+
+export default Details;
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 
 import NavBar from "./NavBar";
 import Content from "./Content";
+import Details from "./Details";
 
 const Layout = () => {
     return (
         <>
             <NavBar />
             <Content />
+            <Details />
             <Outlet />
         </>
     );
