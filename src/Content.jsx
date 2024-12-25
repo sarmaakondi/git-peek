8b00439eeeaaa3a297a8d666f58ece1f0774fad3
sarmaakondi <avnmsarma@gmail.com>
2024-12-25 16:28:06 +1100

    Create Content placeholder component

---
+++ src/Content.jsx
@@ -0,0 +1,9 @@
+const Content = () => {
+    return (
+        <div className="flex flex-col p-8 mt-12 mb-12 border border-black w-80">
+            <h1>Content</h1>
+        </div>
+    );
+};
+
+export default Content;
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
+
 import NavBar from "./NavBar";
+import Content from "./Content";
 
 const Layout = () => {
     return (
         <>
             <NavBar />
+            <Content />
             <Outlet />
         </>
     );
